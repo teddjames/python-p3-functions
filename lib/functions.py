90bc24d22ef377d6b19aa4b4e01f3d72c2551179
Tedd James <tedd.james@student.moringaschool.com>
2025-05-05 12:44:09 +0300

    Functions in Python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print("Hello, Guido!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+    print(add(45,55))
 
 def halve(number):
-    pass
+    return number/2
+    print(halve(100))
